340f492ec80650cbc95ec6cf3d37d9c76e047b93
sigmanor <sigmanor@gmail.com>
2025-01-30 19:36:01 +0200

    feat: add main.go file for Chappie bot

---
+++ cmd/bot/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"chappie_bot/config"
+	"chappie_bot/cron"
+	"chappie_bot/helpers"
+	"chappie_bot/telegram/handlers"
+	"chappie_bot/telegram/middlewares"
+
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithMiddlewares(middlewares.LoggingMiddleware),
+		bot.WithDefaultHandler(handlers.DefaultHandler),
+	}
+
+	helpers.CreateDirIfNotExist("tmp/gh_project_img")
+
+	b, err := bot.New(config.BOT_TOKEN, opts...)
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	handlers.RegisterCommands(b)
+
+	cron.SendMessageCron(ctx, b)
+	cron.CollectPostsCron(ctx, b)
+
+	log.Printf("Bot successfully started ^_^ (app version: %s)\n\n", config.APP_VERSION)
+
+	b.Start(ctx)
+}
