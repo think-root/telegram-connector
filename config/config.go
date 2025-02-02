fac5e54b31a1617b93b5a9fa0d8c710b9096a458
sigmanor <sigmanor@gmail.com>
2025-02-02 12:21:41 +0200

    chore: update app version to 1.0.3
    - Bump the APP_VERSION constant to 1.0.3
    - Add the WAPP_ENABLE environment variable to enable/disable the WhatsApp integration functionality

--- config/config.go
+++ config/config.go
@@ -4,15 +4,24 @@ import (
 	"strconv"
 )
 
-const APP_VERSION = "1.0.2"
+const APP_VERSION = "1.0.3"
 
 var (
 	BOT_TOKEN             = Env("BOT_TOKEN")
 	ADMIN_ID, _           = strconv.ParseInt(Env("ADMIN_ID"), 10, 64)
 	CHANNEL_ID            = Env("CHANNEL_ID")
 	CHAPPIE_SERVER_URL    = Env("CHAPPIE_SERVER_URL")
-	CHAPPIE_SERVER_BEARER = Env(("CHAPPIE_SERVER_BEARER"))
+	CHAPPIE_SERVER_BEARER = Env("CHAPPIE_SERVER_BEARER")
 	WAPP_TOKEN            = Env("WAPP_TOKEN")
 	WAPP_JID              = Env("WAPP_JID")
 	WAPP_SERVER_URL       = Env("WAPP_SERVER_URL")
+	WAPP_ENABLE           = func() bool {
+		if val := Env("WAPP_ENABLE"); val != "" {
+			parsed, err := strconv.ParseBool(val)
+			if err == nil {
+				return parsed
+			}
+		}
+		return false
+	}()
 )
