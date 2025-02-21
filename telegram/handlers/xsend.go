e68f9704a6ef6aa013c88a370c885356edb7fe8d
sigmanor <sigmanor@gmail.com>
2025-02-21 19:29:06 +0200

    feat: implement SendPostToXHandler for sending posts to X (Twitter)

---
+++ telegram/handlers/xsend.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"chappie_bot/config"
+	"chappie_bot/helpers"
+	"chappie_bot/repository"
+	"chappie_bot/x"
+	"context"
+	"log"
+	"strings"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
+func SendPostToXHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	userID := update.Message.From.ID
+	if userID == config.ADMIN_ID {
+		repo, err := repository.GetRepository(1, true)
+		if err != nil {
+			log.Printf("Error getting repository: %v", err)
+			return
+		}
+
+		if len(repo.Data.Items) == 0 {
+			return
+		}
+
+		item := repo.Data.Items[0]
+		username_repo := strings.TrimPrefix(item.URL, "https://github.com/")
+		image_name := "./tmp/gh_project_img/image.png"
+
+		err = repository.Socialify(username_repo)
+		if err != nil {
+			log.Println(err)
+			err := helpers.CopyFile("./assets/github_octopus_logo.png", "./tmp/gh_project_img/image.png")
+			if err != nil {
+				log.Fatalf("Failed to copy file: %v", err)
+			}
+		}
+
+		x_posted := x.CreateXPost(item.Text, item.URL, image_name)
+		if x_posted {
+			log.Println("Message successfully sent to X")
+		} else {
+			log.Println("Message not sent to X")
+		}
+
+		err = helpers.RemoveAllFilesInFolder("./tmp/gh_project_img")
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
