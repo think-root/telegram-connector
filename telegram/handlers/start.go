3dbcfdf6015fd7ac5879826004957d2bb0df0d66
sigmanor <sigmanor@gmail.com>
2025-03-04 12:58:28 +0200

    feat(telegram): add StartHandler to respond with GitHub Ukraine channel link

--- telegram/handlers/start.go
+++ telegram/handlers/start.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-telegram/bot/models"
 )
 
+// StartHandler responds to the /start command by sending a link to the GitHub Ukraine Telegram channel
 func StartHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
